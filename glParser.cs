a4e27401a938942f2fb91b8c25f2c4f935060421
Luis Guijarro Pérez <luisgp82@hotmail.es>
2020-08-07 03:55:00 +0200

    Creamos Clase de Parseo en la que se unifica el parseo y la generación de archivos de salida.

---
+++ glParser.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Xml;
+
+namespace OpenGLParser
+{
+    public static class glParser
+    {
+        private static XmlDocument xdoc;
+        public static void Parse(string rutaxml, string @namespace, string destination, bool verbose)
+        {
+            xdoc = new XmlDocument();
+            xdoc.Load(rutaxml);
+
+            //Procesar Parseo fase a fase.
+            glReader.Parse(xdoc, verbose);
+        }
+    }
+}
