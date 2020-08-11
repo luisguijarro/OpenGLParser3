932258f4873365572775aea7e88d018f23deaa2d
Luis Guijarro Pérez <luisgp82@hotmail.es>
2020-08-11 15:55:17 +0200

    Añadimos llamadas a metodos de escritura de archivos de metodos de OpenGL.

--- glWriter.cs
+++ glWriter.cs
@@ -8,6 +8,12 @@ namespace OpenGLParser
         public static void Write(string NameSpace, string outpath, bool verbose)
         {
             WriteEnums(NameSpace, outpath, verbose);
+            WriteCommands(NameSpace, outpath, verbose);
+            WriteInternals(NameSpace, outpath, verbose);
+            WriteDelegates(NameSpace, outpath, verbose);
+            WriteInternalTools(NameSpace, outpath, verbose);
+            WriteDelegateInitializer(NameSpace, outpath, verbose);
+
         }
     }
 }
