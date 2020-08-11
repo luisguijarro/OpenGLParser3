f16e80740b44589dbf18f4a7db0039c8917d6b4b
Luis Guijarro Pérez <luisgp82@hotmail.es>
2020-08-11 15:48:27 +0200

    Creamos lector de versiones de OpenGL.

---
+++ Reader/VersionReader.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Xml;
+using System.Collections.Generic;
+
+using OpenGLParser.DataObjects;
+
+namespace OpenGLParser
+{
+    public static partial class glReader
+    {
+        public static Dictionary<string,glVersion> d_versiones; //<version, glversion>
+        private static void ReadVersions(XmlDocument xdoc, bool verbose)
+        {
+            d_versiones = new Dictionary<string, glVersion>();
+            if (verbose) { Console.WriteLine(); Console.WriteLine("Parsing OpenGL Versions."); }
+
+            XmlNodeList versionlist = xdoc.SelectNodes("registry/feature[@api='gl']"); //Obtenemos lista de Versiones
+            if (versionlist.Count > 0) //Comprobamos que se obtienen resultados.
+            {
+                for (int i = 0; i < versionlist.Count; i++) //Recorremos las Versiones
+                {
+                    string s_versionNumber = versionlist[i].Attributes["number"].Value; //Obtenemos el número de versión.
+                    glVersion verTemp = new glVersion();
+
+                    XmlNodeList MetodosEnVersion = versionlist[i].SelectNodes("require/command"); //Obtenemos lista de metodos soportados
+                    for (int m=0;m<MetodosEnVersion.Count;m++) //Recorremos lista de metodos soportados.
+                    {
+                        verTemp.Metodos.Add(MetodosEnVersion[m].Attributes["name"].Value); //Añadimos metodo a la lista de soportados.
+                        if (Commandos.ContainsKey(MetodosEnVersion[m].Attributes["name"].Value)) //Comprobanmos que el método existe.
+                        {
+                            if (Commandos[MetodosEnVersion[m].Attributes["name"].Value].FromVersion != "") //Comprovamos si ha existido antes.
+                            {
+                                Commandos[MetodosEnVersion[m].Attributes["name"].Value].FromVersion = s_versionNumber; //Establecemos esta versión de OpenGL como la primera
+                            }
+                        }
+                    }
+
+                    XmlNodeList MetodosEliminados = versionlist[i].SelectNodes("remove/command"); //Obtenemos lista de metodos obsoletos.
+                    for (int r=0;r<MetodosEliminados.Count;r++) //Recorremos lista de metodos obsoletos.
+                    {
+                        verTemp.Obsoletos.Add(MetodosEliminados[r].Attributes["name"].Value); //Añadimos metodo a la lista de obsoletos.
+                        if (Commandos.ContainsKey(MetodosEliminados[r].Attributes["name"].Value)) //Comprobanmos que el método existe.
+                        {
+                            if (Commandos[MetodosEliminados[r].Attributes["name"].Value].DeprecatedVersion != "") //Comprovamos si se ha depreciado anteriormente
+                            {
+                                Commandos[MetodosEliminados[r].Attributes["name"].Value].DeprecatedVersion = s_versionNumber; //Se marca como obsoleta a partir de esta versión.
+                            }
+                        }
+                    }
+                    d_versiones.Add(s_versionNumber, verTemp); //Añadimos Version a la lista de versiones.
+                }
+            }
+
+            if (verbose) //Mostrar Recuento final.
+            {
+                /*Console.SetCursorPosition(0,ctop);
+                Console.Write(new String(' ', Console.BufferWidth)); //Limpiamos linea a sobreescribir.
+                Console.SetCursorPosition(0,ctop);*/
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Parsed ");
+                Console.ResetColor(); 
+                Console.WriteLine(d_versiones.Count + " OpenGL Versions.");
+            }
+        }
+    }
+}
