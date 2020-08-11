769e4456ed6b289f0cce76d0894e22fe5fd4dd10
Luis Guijarro Pérez <luisgp82@hotmail.es>
2020-08-11 15:57:36 +0200

    Añadimos control de versiones en los metodos.

--- DataObjects/glCommand.cs
+++ DataObjects/glCommand.cs
@@ -5,11 +5,15 @@ namespace OpenGLParser.DataObjects
 {
     public class glCommand
     {
-        public string ReturnedTipe;
+        public string FromVersion; //Versión desde la que aparece
+        public string DeprecatedVersion; //Versión en la que se marca como Obsoleto
+        public string ReturnedTipe; //Tipo de dato que retorna.
         public Dictionary<string, glParam> Parametros; //<nombre del parametro, Parametro>,
         public glCommand()
         {
-            ReturnedTipe = "void";
+            FromVersion = "";
+            DeprecatedVersion = "";
+            ReturnedTipe = "void"; //void por defecto.
             this.Parametros = new Dictionary<string, glParam>();
         }
     }
