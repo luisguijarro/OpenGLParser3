b62cb4a17d85e851f46c2a442fd9c03611a4daa5
Luis Guijarro Pérez <luisgp82@hotmail.es>
2020-08-11 15:58:21 +0200

    Añadimos control de grupo no incluido en grupos del xml.

--- DataObjects/glEnumValue.cs
+++ DataObjects/glEnumValue.cs
@@ -6,10 +6,12 @@ namespace OpenGLParser.DataObjects
     {
         public string Name;
         public string Value;
-        public glEnumValue(string name, string value)
+        public string Group;
+        public glEnumValue(string name, string value, string group)
         {
             Name = name;
             Value = value;
+            Group = group;
         }
     }
 }
