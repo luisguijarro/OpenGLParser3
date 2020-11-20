d448b7ae3fe7f9cbe5a514c06f9f4d46fa6f5a24
Luis Guijarro Pérez <luisgp82@hotmail.es>
2020-11-20 09:43:16 +0100

    Añadimos Atributo para control de tipo de acceso del parametro.

--- DataObjects/glParam.cs
+++ DataObjects/glParam.cs
@@ -5,15 +5,23 @@ namespace OpenGLParser.DataObjects
 {
     public class glParam
     {
+        public AccesParam Acces; // Tipo de Acceso al Parametro, A emplear para punteros.
         public string tipo;
         // public int len; //Si es un Array tiene 0 o más
         public bool esArray;
         public int esPuntero; //cambiamos bool por int para contar el numero de *
         public glParam()
         {
+            Acces = AccesParam.Unknown;
             tipo = "";
             //this.len = -1; //Si es un Array tiene 0 o más
             esArray = false;
         }
+    }
+
+
+    public enum AccesParam
+    {
+        Unknown = 0, In, Out, Ref
     }
 }
