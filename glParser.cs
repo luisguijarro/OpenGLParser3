c9b6c5a924e70a052c5cd8d2a8742735330f9239
Luig Guijarro Pérez <luisgp82@hotmail.es>
2022-01-04 03:05:22 +0100

    Pasamos parametro de si se debe parsear OpenGL|ES.

--- glParser.cs
+++ glParser.cs
@@ -6,13 +6,13 @@ namespace OpenGLParser
     public static class glParser
     {
         private static XmlDocument xdoc;
-        public static void Parse(string rutaxml, string @namespace, string destination, bool verbose, bool gitRefPages)
+        public static void Parse(string rutaxml, string @namespace, string destination, bool verbose, bool gitRefPages, bool ogles)
         {
             xdoc = new XmlDocument();
             xdoc.Load(rutaxml);
 
             //Procesar Parseo fase a fase.
-            glReader.Parse(xdoc, verbose);
+            glReader.Parse(xdoc, verbose, ogles);
 
             if (gitRefPages)
             {
