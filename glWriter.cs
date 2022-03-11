8d0e46e625ac461ed001d9902133d6cd1ebd8072
Luig Guijarro Pérez <luisgp82@gmail.com>
2022-03-11 02:28:19 +0100

    Añadimos llamadas a escritura de OpenGL|ES.

--- glWriter.cs
+++ glWriter.cs
@@ -5,7 +5,7 @@ namespace OpenGLParser
 {
     public static partial class glWriter
     {
-        public static void Write(string NameSpace, string outpath, bool verbose)
+        public static void Write(string NameSpace, string outpath, bool verbose, bool ogles)
         {
             WriteEnums(NameSpace, outpath, verbose);
             WriteInternals(NameSpace, outpath, verbose);
@@ -15,6 +15,21 @@ namespace OpenGLParser
             WriteDelegateInitializerEXT(NameSpace, outpath, verbose);
             WriteCommands(NameSpace, outpath, verbose);
             WriteEXTCommands(NameSpace, outpath, verbose);
+
+            if (ogles)
+            {
+                if (!System.IO.Directory.Exists(outpath+"/gles"))
+                {
+                    System.IO.Directory.CreateDirectory(outpath+"/gles");
+                }
+                WriteGLesInternals(NameSpace, outpath+"/gles/", verbose);
+                WriteGlesDelegates(NameSpace, outpath+"/gles/", verbose);
+                WriteInternalGLesTools(NameSpace, outpath+"/gles/", verbose);
+                WriteDelegateInitializerGles(NameSpace, outpath+"/gles/", verbose);
+                WriteDelegateInitializerGLesEXT(NameSpace, outpath+"/gles/", verbose);
+                WriteGlesCommands(NameSpace, outpath+"/gles/", verbose);
+                WriteGlesEXTCommands(NameSpace, outpath+"/gles/", verbose);
+            }
         }
     }
 }
